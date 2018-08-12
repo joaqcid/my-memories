098311ef71ae4ee736e2518e157c50ee275bc8c6
Joaquin Cid <joaqcid@gmail.com>
2018-08-12 19:28:11 -0300

    last changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
   production: true,
-  hmr: false
+  hmr: false,
+  firebase:{
+    apiKey: "AIzaSyAs6gxDfcbsdgslPkbhbmgjNbbcgqR3m_g",
+    authDomain: "my-memories-b4c52.firebaseapp.com",
+    databaseURL: "https://my-memories-b4c52.firebaseio.com",
+    projectId: "my-memories-b4c52",
+    storageBucket: "my-memories-b4c52.appspot.com",
+    messagingSenderId: "695101077060"
+  }
  };
